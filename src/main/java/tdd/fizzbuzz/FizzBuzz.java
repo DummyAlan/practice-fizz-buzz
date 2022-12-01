4ffd5ab6526e1fa198caf874304ea3cb03397b3e
Alan Wan <alan.wan@oocl.com>
2022-12-01 14:14:11 +0800

    Feat: finished testcases 1 and 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String countOff(int number){
+
+        String resultStr = "";
+
+        if (number%3 == 0){
+            resultStr = "Fizz";
+        }
+        else {
+            resultStr = String.valueOf(number);
+        }
+        return resultStr;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,24 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    FizzBuzz mFizzBuzz = new FizzBuzz();
+
+    @Test
+    public void given1_whenCountOff_thenShouldReturn1() {
+        String actual = mFizzBuzz.countOff(1);
+
+        assertEquals(actual, "1");
+    }
+    @Test
+    public void given3_whenCountOff_thenShouldReturnFizz() {
+        String actual = mFizzBuzz.countOff(3);
+
+        assertEquals(actual, "Fizz");
+    }
+
 }
